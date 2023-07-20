27240b0c818ab57524e0b886a208fee539ad90b3
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-07-20 16:37:00 +0300

    add creet comonent

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header/Header";
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import { Create } from "./components/Create/Create.jsx"
 import { Register } from "./components/Register/Register";
 import { Login } from "./components/Login/Login";
-import { Dashboard } from "./components/Dashboard/Dashboard";
 import { Edit } from "./components/Edit/Edit";
 import { Details } from "./components/Details/Details.jsx"
 
@@ -18,6 +19,7 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />}/>
               <Route path="/register" element={<Register />}/>
               <Route path="/login" element={<Login />}/>
+              <Route path="/create" element={<Create />}/>
               <Route path="/edit/:id" element={<Edit />}/>
               <Route path="/details/:id" element={<Details />}/> 
           </Routes>
