af67a5da4f00134c40b55908e20e76468755318d
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-07-20 16:22:08 +0300

    import all components into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import { Home } from "./components/Home/Home";
 import { Footer } from "./components/common/Footer/Footer";
 import { Header } from "./components/common/Header";
+import { Register } from "./components/Register/Register";
+import { Login } from "./components/Login/Login";
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import { Edit } from "./components/Edit/Edit";
+import { Details } from "./components/Details/Details.jsx"
 
 function App() {
   return (
