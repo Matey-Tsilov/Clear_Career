fba194a87af8469fde4d57c8544a502b80d7a940
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-07-31 15:01:09 +0300

    create custom hook -> useState on steroids

--- client/src/contexts/userContext.js
+++ client/src/contexts/userContext.js
@@ -1,10 +1,12 @@
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
 const { createContext, useState } = require("react");
 
 export const UserContext = createContext()
 
 export const UserContextProvider = ({children}) => {
     
-    const [user, setUser] = useState({})
+    const [user, setUser] = useLocalStorage({})
 
     return (
         <UserContext.Provider value={{user, setUser}}>
--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,3 +1,25 @@
-const useLocalStorage = () => {
-    
+import { useState } from "react"
+import { getUserData, clearUserData, setUserData } from "../api/util"
+
+export const useLocalStorage = (defaultUser) => {
+    const [user, setUser] = useState(() => {
+        const loggedUser = getUserData()
+
+        if (loggedUser?.userData) {
+            return loggedUser
+        }
+        return defaultUser
+    })
+
+const setLocalStorage = (curUser = '') => {
+    if (curUser) {
+        setUser(curUser)
+        setUserData(curUser)
+    }else {
+        setUser({})
+        clearUserData()
+    }
+}
+
+    return [user, setLocalStorage]
 }
