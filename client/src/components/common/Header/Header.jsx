73d96368b30e1f0e12f896bebbc2556552ac4b57
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-08-22 14:59:35 +0300

    togle viewing of login and register btns

--- client/src/components/common/Header/Header.jsx
+++ client/src/components/common/Header/Header.jsx
@@ -5,36 +5,41 @@ import logo from "../../../assets/logo.jpg";
 
 import { UserContext } from "../../../contexts/userContext.js";
 import { useContext } from "react";
-import { Notification } from '../Notification/Notification'
+import { Notification } from "../Notification/Notification";
 import { NotifyContext } from "../../../contexts/notificationContext";
 
 export const Header = () => {
   const { user, setUser } = useContext(UserContext);
-  const {notify} = useContext(NotifyContext)
-  
+  const { notify } = useContext(NotifyContext);
+
   return (
     <header>
       {/* Navigation */}
-      <Link to='/' id="logo">
+      <Link to="/" id="logo">
         <img id="logo-img" src={logo} alt="" />
       </Link>
       <nav>
         <div className="guest-specific">
-          <Link to="/profile">Welcome, <span>{user.email || "Guest"}</span></Link>
+          <Link to="/profile">
+            Welcome, <span>{user.email || "Guest"}</span>
+          </Link>
           <Link to="/dashboard">Dashboard</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
-
         </div>
-        {Object.values(user) != 0 && 
-        (
+        {Object.values(user) != 0 ? (
           <div className={style.user}>
             <Link to="/create">Create Offer</Link>
-            <Link onClick={() => setUser()} to="/dashboard">Logout</Link>
+            <Link onClick={() => setUser()} to="/dashboard">
+              Logout
+            </Link>
           </div>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
         )}
       </nav>
-      {notify.opened && <Notification message={notify.msg}/>}
+      {notify.opened && <Notification message={notify.msg} />}
     </header>
   );
 };
